848f76d7ae77020ac6843c5ebf98f8ad2be62434
bellaqamendim@gmail.com <bellaqamendim@gmail.com>
2023-06-18 18:33:45 +0200

    Stattus added in task card - fromt

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -198,105 +198,109 @@
         </nav>
 
         <div class="relative">
-
-          <div style="width: 100%; position: absolute;" class="bg-cover bg-center bg-yellow-600 items-center justify-around content-start min-h-screen">
-            <!-- tasks -->
-            <!-- Hero section -->
-            @if(auth()->check())
-            <div class="flex items-center justify-center flex-col bg-[#E5E5E5] ">
-              <a href="{{ route('tasks.create') }}" class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-8">
-                Krijo Detyrë
-              </a>
-            </div>
-            @endif
-
-            <div id="task-container" class="grid grid-cols-3 gap-4 flex items-center justify-center mt-8 mb-16 ml-5 mr-5">
-              @forelse ($tasks as $task)
-              @if(auth()->user()->role == 1 || ($task->user_id == auth()->user()->id && auth()->user()->role == 0))
-              <div class="card rounded-lg bg-orange-200 p-4 w-full mx-auto mb-4">
-                <img src="https://cdn-icons-png.flaticon.com/512/351/351501.png?w=740&t=st=1684714193~exp=1684714793~hmac=11791ab8c8f7af7f9a0aa076fa6013c8da75ff97d18a7b67c1dbf3153a492915" class="w-12">
-                <div class="mt-3 text-gray-800 font-semibold text-lg">{{ $task->title }}</div>
-                <div class="text-sm text-gray-800 font-light">
-                  @if (strlen($task->description) > 50)
-                  <div>
-                    <p>{{ substr($task->description, 0, 50) }}</p>
-                    <button id="show-description-btn-{{ $task->id }}" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1 px-2 rounded mt-2 focus:outline-none">
-                      Shfaq të plotë
-                    </button>
-                    <div id="description-{{ $task->id }}" class="description hidden">
-                      {{ $task->description }}
-                    </div>
-                  </div>
-                  @else
-                  {{ $task->description }}
-                  @endif
-                </div>
-                <p>{{ $task->user->name }}</p>
-                <div class="my-4">
-                  <p class="due-time font-bold text-gray-800 text-base">Koha fundit: {{ $task->due_date }}</p>
-                  <p class="due-time font-bold text-gray-800 text-base">Kan mbetur: {{ \Carbon\Carbon::parse($task->due_date . ' ' . $task->due_time)->diffForHumans() }}</p>
-                  <span class="font-light text-gray-800 text-sm">
-                    <a href="{{ route('tasks.edit', $task->id) }}" class="btn btn-primary text-green-600 bold">{{ __('Ndrysho') }}</a>
-                    <form method="POST" action="{{ route('tasks.destroy', $task->id) }}" style="display: inline-block;">
-                      @csrf
-                      @method('DELETE')
-                      <button type="submit" class="btn btn-danger text-red-600 ">{{ __('Fshij') }}</button>
-                    </form>
-                  </span>
-                </div>
-                <select id="status-dropdown-{{ $task->id }}">
-                    <!-- Vendosni opsionet e dropdown-it këtu -->
-                    <option value="completed">Completed</option>
-                    <option value="in_progress">In Progress</option>
-                    <option value="pending">Pending</option>
-                  </select>
-                  <button onclick="onStatusChange({{ $task->id }})">Save</button>
+            <div style="width: 100%; position: absolute;" class="bg-cover bg-center bg-yellow-600 items-center justify-around content-start min-h-screen">
+              <!-- tasks -->
+              <!-- Hero section -->
+              @if(auth()->check())
+              <div class="flex items-center justify-center flex-col bg-[#E5E5E5] ">
+                <a href="{{ route('tasks.create') }}" class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-8">
+                  Krijo Detyrë
+                </a>
               </div>
               @endif
-              @empty
-              <div class="bg-[#F9ECFF] rounded-xl">
-                <p class="text-gray-600">Nuk ka detyra në dispozicion.</p>
+
+              <div id="task-container" class="grid grid-cols-3 gap-4 flex items-center justify-center mt-8 mb-16 ml-5 mr-5">
+                @forelse ($tasks as $task)
+                @if(auth()->user()->role == 1 || ($task->user_id == auth()->user()->id && auth()->user()->role == 0))
+                <div class="card rounded-lg bg-orange-200 p-4 w-full mx-auto mb-4">
+                  <img src="https://cdn-icons-png.flaticon.com/512/351/351501.png?w=740&t=st=1684714193~exp=1684714793~hmac=11791ab8c8f7af7f9a0aa076fa6013c8da75ff97d18a7b67c1dbf3153a492915" class="w-12">
+                  <div class="mt-3 text-gray-800 font-semibold text-lg">{{ $task->title }}</div>
+                  <div class="text-sm text-gray-800 font-light">
+                    @if (strlen($task->description) > 50)
+                    <div>
+                      <p>{{ substr($task->description, 0, 50) }}</p>
+                      <div class="relative">
+                        <div class="px-2 py-1 mt-2">
+                          <button id="show-description-btn-{{ $task->id }}" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1 px-2 rounded mt-2 focus:outline-none">
+                            Shfaq të plotë
+                          </button>
+                        </div>
+                        <button class="bg-gray-800 text-white hover:bg-green-700 font-bold py-2 px-3 rounded mt-2 absolute right-0 top-0">{{ $task->status }}</button>
+                      </div>
+                      <div id="description-{{ $task->id }}" class="description hidden">
+                        {{ $task->description }}
+                      </div>
+                    </div>
+                    @else
+                    {{ $task->description }}
+                    @endif
+                  </div>
+                  <p>{{ $task->user->name }}</p>
+                  <div class="my-4">
+                    <p class="due-time font-bold text-gray-800 text-base">Koha fundit: {{ $task->due_date }}</p>
+                    <p class="due-time font-bold text-gray-800 text-base">Kan mbetur: {{ \Carbon\Carbon::parse($task->due_date . ' ' . $task->due_time)->diffForHumans() }}</p>
+                    <span class="font-light text-gray-800 text-sm">
+                      <a href="{{ route('tasks.edit', $task->id) }}" class="btn btn-primary text-green-600 bold">{{ __('Ndrysho') }}</a>
+                      <form method="POST" action="{{ route('tasks.destroy', $task->id) }}" style="display: inline-block;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger text-red-600">{{ __('Fshij') }}</button>
+                      </form>
+                    </span>
+                  </div>
+                  <div class="relative">
+                    <select id="status-dropdown-{{ $task->id }}" class="border border-gray-400 rounded-md px-2 py-1 mt-2">
+                      <!-- Vendosni opsionet e dropdown-it këtu -->
+                      <option value="completed">Completed</option>
+                      <option value="in_progress">In Progress</option>
+                      <option value="pending">Pending</option>
+                    </select>
+                    <button onclick="onStatusChange({{ $task->id }})" class="bg-black text-white hover:bg-green-700 font-bold py-2 px-3 rounded mt-2 absolute right-0 top-0">Save</button>
+                  </div>
+                </div>
+                @endif
+                @empty
+                <div class="bg-[#F9ECFF] rounded-xl">
+                  <p class="text-gray-600">Nuk ka detyra në dispozicion.</p>
+                </div>
+                @endforelse
               </div>
-              @endforelse
             </div>
           </div>
-            </div>
-        </div>
 
-    </div>
 
 
     <script>
-  var csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+        var csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
-var xhr = new XMLHttpRequest();
-xhr.open("POST", "/tasks/" + taskId + "/update-status", true);
-xhr.setRequestHeader("Content-Type", "application/json");
-xhr.setRequestHeader("X-CSRF-TOKEN", csrfToken); // Shtoni këtë linjë për të shtuar tokenin CSRF në kërkesë
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        console.log(xhr.responseText); // Response message from the controller
-      } else {
-        console.error("Error while saving the status to the database.");
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "/tasks/" + taskId + "/update-status", true);
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.setRequestHeader("X-CSRF-TOKEN", csrfToken); // Shtoni këtë linjë për të shtuar tokenin CSRF në kërkesë
+      xhr.onreadystatechange = function() {
+          if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+              console.log(xhr.responseText); // Response message from the controller
+            } else {
+              console.error("Error while saving the status to the database.");
+            }
+          }
+        };
+
+        var data = JSON.stringify({ newStatus: newStatus });
+        xhr.send(data);
       }
-    }
-  };
 
-  var data = JSON.stringify({ newStatus: newStatus });
-  xhr.send(data);
-}
+      // Complete the onStatusChange function with code that displays the dropdown and updates the task status in the database
+      function onStatusChange(taskId) {
+        var dropdown = document.getElementById("status-dropdown-" + taskId);
+        var selectedStatus = dropdown.value;
 
-// Complete the onStatusChange function with code that displays the dropdown and updates the task status in the database
-function onStatusChange(taskId) {
-  var dropdown = document.getElementById("status-dropdown-" + taskId);
-  var selectedStatus = dropdown.value;
+        // Update the status in the database
+        saveStatusToDatabase(taskId, selectedStatus);
+      }
 
-  // Update the status in the database
-  saveStatusToDatabase(taskId, selectedStatus);
-}
-
-      </script>
+    </script>
 </body>
 
 </html>
